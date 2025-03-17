1fcdcf5d202fde51ea27e9b601c38126367e48cc
cavinato <87499876+cavinatto@users.noreply.github.com>
2025-03-17 13:33:50 -0300

    Mudanças para os devidos testes unitários

--- Flask_API/app.py
+++ Flask_API/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from routes import bp
 
-app = Flask(__name__)
-app.register_blueprint(bp)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(bp)
+    return app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
