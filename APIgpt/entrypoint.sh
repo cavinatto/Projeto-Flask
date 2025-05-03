a6ad7865afaf7d5d15edf067a2ac078fec7862a5
cavinato <87499876+cavinatto@users.noreply.github.com>
2025-05-03 19:04:15 -0300

    Update entrypoint.sh

--- APIgpt/entrypoint.sh
+++ APIgpt/entrypoint.sh
@@ -6,5 +6,5 @@ until mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" 
   sleep 2
 done
 
-echo "MySQL está pronto! Iniciando a aplicação Flask..."
-flask run --host=0.0.0.0 --port=8000
+echo "MySQL está pronto! Iniciando a aplicação Flask com gunicorn..."
+exec gunicorn -b 0.0.0.0:${PORT:-8000} app:app
