a1c6030e5a58d1540e8100fd699b7066eb84b502
cavinato <87499876+cavinatto@users.noreply.github.com>
2025-05-04 14:39:32 -0300

    Update app.py

--- APIgpt/app.py
+++ APIgpt/app.py
@@ -1,13 +1,13 @@
 from config import app, db
-from alunos.alunos_routes import alunos_blueprint
-from professores.professores_routes import professores_blueprint
-from turmas.turmas_routes import turmas_blueprint
+#from alunos.alunos_routes import alunos_blueprint
+#from professores.professores_routes import professores_blueprint
+#from turmas.turmas_routes import turmas_blueprint
 
 from swagger.swagger_config import configure_swagger
 
-app.register_blueprint(alunos_blueprint, url_prefix='/api')
-app.register_blueprint(professores_blueprint, url_prefix='/api')
-app.register_blueprint(turmas_blueprint, url_prefix='/api')
+#app.register_blueprint(alunos_blueprint, url_prefix='/api')
+#app.register_blueprint(professores_blueprint, url_prefix='/api')
+#app.register_blueprint(turmas_blueprint, url_prefix='/api')
 
 configure_swagger(app)
 
