578a597a8d5fd1226651f05d518ded775aede6e8
cavinato <87499876+cavinatto@users.noreply.github.com>
2025-05-04 14:22:43 -0300

    Update entrypoint.sh

--- APIgpt/entrypoint.sh
+++ APIgpt/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Espera o MySQL estar pronto
-until mysql -h db -u root -pEnzo39824360 -e "SELECT 1"; do
-  echo "Aguardando o MySQL iniciar..."
-  sleep 1
-done
+if [[ "$FLASK_ENV" != "production" ]]; then
+  # Espera o MySQL iniciar apenas em desenvolvimento/local
+  until mysql -h db -u root -pEnzo39824360 -e "SELECT 1"; do
+    echo "Aguardando o MySQL iniciar..."
+    sleep 1
+  done
+  echo "MySQL está pronto!"
+fi
 
-echo "MySQL está pronto! Iniciando a aplicação Flask..."
+echo "Iniciando a aplicação Flask..."
 flask run --host=0.0.0.0 --port=8000
